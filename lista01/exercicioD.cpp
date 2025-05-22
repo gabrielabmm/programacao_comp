603a1b579ac3e62e9ec11e70c4242971b22300df
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-05-22 15:27:47 -0300

    Create exercicioD.cpp

---
+++ lista01/exercicioD.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int y;
+
+cin >> y;
+
+if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) {
+    cout << "366";
+}
+
+else { cout << "365";
+}
+
+return 0;
+} 
