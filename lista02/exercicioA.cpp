3a7be6d35aab2fcb60c4d4d61fc89e7b16c891a5
gabriela brasileiro <gabrielabrasileiro@discente.ufg.br>
2025-05-22 15:29:46 -0300

    Create exercicioA.cpp

---
+++ lista02/exercicioA.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+int A, B;
+    cin >> A;
+
+    B = 400 / A ;
+
+    if (400 % A == 0){
+        cout << B;} 
+    else { cout << "-1";}
+
+return 0;
+}
